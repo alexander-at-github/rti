a0c66e164fb304bdf7747168c0705b49d6ba2b71
Alexander Scharinger <alexander.at.github@gmail.com>
2019-05-07 12:03:23 +0200

    Ray Tracing working state

--- src/rti/TestRTIntersect.cpp
+++ src/rti/TestRTIntersect.cpp
@@ -10,11 +10,13 @@
 #include <embree3/rtcore.h>
 #include <gmsh.h>
 #include <pmmintrin.h>
-#include <string>
 #include <xmmintrin.h>
 
-#include <assert.h>
+#include <algorithm>
 #include <iostream>
+#include <string>
+
+#include <assert.h>
 
 namespace blt = boost::log::trivial;
 namespace bls = boost::log::sources;
@@ -58,7 +60,7 @@ int main(int argc, char* argv[]) {
     //inpath = "../resources/test.cyl.msh";
     // Gmsh says 14 nodes and 44 elements for box.msh
     // Gmsh says 314 nodes, 704 elements, 8 points, 72 lines, and 624 triangles for box.fine.msh
-    inpath = "../resources/box.fine.msh";
+    inpath = "/home/alexander/cdr/rti/resources/box.fine.msh";
   }
   std::cout << "Reading input file " << inpath << std::endl;
   // Read gmsh fille from arguments
@@ -74,7 +76,7 @@ int main(int argc, char* argv[]) {
   //////////////////////////////////////////////////////////////////////////////
 
   { // Debug
-    BOOST_LOG_SEV(man.lg, blt::debug) << "gmsh::model::mesh::getElemets():";
+    BOOST_LOG_SEV(man.lg, blt::debug) << "gmsh::model::mesh::getElements():";
     // Do we need a new model with gmsh::model::add("<name>") first.
     // Do we need gmsh::model::geo::synchronize()?
     std::vector<std::size_t> vvtags;
@@ -91,10 +93,35 @@ int main(int argc, char* argv[]) {
     // need points only (dimension 0) and triangles (dimension 2).
     // gmsh::model::mesh::getElements() does not return the parametric
     // coordinates. We need gmsh::model::mesh::getNodes().
+
+
+    /*********************************************************************************************
+     * When querying Gmash with getElements(eetypes, eetags, nntags) then the content of eetypes *
+     * is a vector of ints. The node tags vector nntags must be indexed by the same index as     *
+     * eetypes. That is, the content of eetypes[idx] is NOT the correct index for nntags. The    *
+     * correct index is the variable idx. eetypes[idx] corresponds to nntags[idx].               *
+     *********************************************************************************************/
+
      
     BOOST_LOG_SEV(man.lg, blt::trace) << "eetypes.size()=" << eetypes.size();
 
-    size_t idx = 0;
+
+    BOOST_LOG_SEV(man.lg, blt::debug) << "##################################################";
+    BOOST_LOG_SEV(man.lg, blt::debug) << "### Using loop with 0 <= idx <= eetypes.size() ###";
+    BOOST_LOG_SEV(man.lg, blt::debug) << "##################################################";
+    for (size_t idx = 0; idx < eetypes.size(); ++idx) {
+      int eetype = eetypes[idx];
+      std::string eename;
+      int eedim;
+      int eeorder;
+      int eenumnodes;
+      std::vector<double> eeparamcoord;
+      gmsh::model::mesh::getElementProperties(eetype, eename, eedim, eeorder, eenumnodes, eeparamcoord);
+      BOOST_LOG_SEV(man.lg, blt::trace) << "idx=" << idx  << " eename=" << eename << " eetype=" << eetype << " eedim=" << eedim << " eenumnodes=" << eenumnodes << "; number of elements of that type (eetags[idx].size()) = " << eetags[idx].size() << " nntags[idx].size()=" << nntags[idx].size();
+    }
+    BOOST_LOG_SEV(man.lg, blt::debug) << "###############################################";
+    BOOST_LOG_SEV(man.lg, blt::debug) << "### Using loop for (auto& eetype : eetypes) ###";
+    BOOST_LOG_SEV(man.lg, blt::debug) << "###############################################";
     for (auto& eetype : eetypes) {
       std::string eename;
       int eedim;
@@ -102,11 +129,35 @@ int main(int argc, char* argv[]) {
       int eenumnodes;
       std::vector<double> eeparamcoord;
       gmsh::model::mesh::getElementProperties(eetype, eename, eedim, eeorder, eenumnodes, eeparamcoord);
-      BOOST_LOG_SEV(man.lg, blt::trace) << "eename=" << eename << ", eedim=" << eedim << ", eenumnodes=" << eenumnodes << "; number of elements of that type (eetags[eetype].size()) = " << eetags[idx].size();
-      ++idx;
+      BOOST_LOG_SEV(man.lg, blt::trace) << "eename=" << eename << " eetype=" << eetype << " eedim=" << eedim << " eenumnodes=" << eenumnodes << "; number of elements of that type (eetags[eetype].size()) = " << eetags[eetype].size() << " nntags[eetype].size()=" << nntags[eetype].size();
     }
 
+    {
+      BOOST_LOG_SEV(man.lg, blt::debug) << "##################################################";
+      BOOST_LOG_SEV(man.lg, blt::debug) << "### Using getElements(\"select triangles\")      ###";
+      BOOST_LOG_SEV(man.lg, blt::debug) << "### Using loop with 0 <= idx <= eetypes.size() ###";
+      BOOST_LOG_SEV(man.lg, blt::debug) << "##################################################";
+      std::vector<int> eetypes;
+      std::vector<std::vector<std::size_t>> eetags;
+      std::vector<std::vector<std::size_t>> nntags;
+      const int selecttriangles = 2; // triangles correspond to two dimensions
+      gmsh::model::mesh::getElements(eetypes, eetags, nntags, selecttriangles, -1); // -1: all tags
+      for (size_t idx = 0; idx < eetypes.size(); ++idx) {
+        int eetype = eetypes[idx];
+        std::string eename;
+        int eedim;
+        int eeorder;
+        int eenumnodes;
+        std::vector<double> eeparamcoord;
+        gmsh::model::mesh::getElementProperties(eetype, eename, eedim, eeorder, eenumnodes, eeparamcoord);
+        BOOST_LOG_SEV(man.lg, blt::trace) << "idx=" << idx  << " eename=" << eename << " eetype=" << eetype << " eedim=" << eedim << " eenumnodes=" << eenumnodes << "; number of elements of that type (eetags[idx].size()) = " << eetags[idx].size() << " nntags[idx].size()=" << nntags[idx].size();
+      }
+    }
   } // End Debug
+  BOOST_LOG_SEV(man.lg, blt::debug) << "##############################";
+  BOOST_LOG_SEV(man.lg, blt::debug) << "### End of Debug section 1 ###";
+  BOOST_LOG_SEV(man.lg, blt::debug) << "##############################";
+
 
   // TODO: Verify the input.
 
@@ -155,23 +206,60 @@ int main(int argc, char* argv[]) {
       sizeof(rti::Utils::Vertex),
       numvertices);
 
+  size_t vvtagsmaxval = *std::max_element(std::begin(vvtags), std::end(vvtags));
+  // The tag with maximal value in vvtags has a number at most equal to the number
+  // of elements in vvtags. That is, tags are numbered consecutively.
+  assert(vvtagsmaxval <= vvtags.size() && "Non-consecutive node tags in the vector vvtags");
+  // tags in vvtags start from the integer 1.
+  size_t vvtagsminval = *std::min_element(std::begin(vvtags), std::end(vvtags));
+  assert(vvtagsminval >= 1 && "vvtags contains a tag with value less than 1");
+
+  // TODO: CONTINUE here
+  // In Gmsh the node tags are strictly positive. In Embree vertices do not have explicite
+  // tags. They are just members of an array. The index of the array starts from zero.
+  // In order to fix this discrepancy we adopt the 
+
   // Write vertices ("nodes" in Gmsh) from Gmsh to Embree
-  for (size_t idx = 0; idx < numvertices; ++idx) {
-    size_t npidx = idx * 3;
-    assert(npidx <= vvxyz.size() && "npidx out of bounds");
-    vvbuffer[idx].xx = vvxyz[npidx];
-    vvbuffer[idx].yy = vvxyz[npidx+1];
-    vvbuffer[idx].zz = vvxyz[npidx+2];
+  for (size_t idx = 0; idx < vvtags.size(); ++idx) {
+    size_t vcidx = 3 * idx;
+    //
+    size_t vvtag = vvtags[idx];
+    // Gmsh uses tags starting from 1.
+    assert(vvtag >= 1);
+    // Arrays are indexed starting from 0. Hence, we subtract one.
+    vvtag -= 1; // FIXME: This is part of a bug!
+    // Note: In Gmsh the tags are strictly positive (starting from 1); at the
+    // same time the array of coordinates (vvxyz) still contains useful data
+    // starting from index 0. That is vvxyz[0]..vvxyz[2] contains the
+    // coordinates for tag 1.
+    vvbuffer[vvtag].xx = vvxyz[vcidx];
+    vvbuffer[vvtag].yy = vvxyz[vcidx+1];
+    vvbuffer[vvtag].zz = vvxyz[vcidx+2];
+    //BOOST_LOG_SEV(man.lg, blt::debug) << "(" << vvbuffer[vvtag].xx << " " << vvbuffer[vvtag].yy << " " << vvbuffer[vvtag].zz << ")" << std::endl;
   }
 
   std::vector<int> eetypes;
   std::vector<std::vector<std::size_t>> eetags;
   std::vector<std::vector<std::size_t>> nntags;
-  // TODO: Can one use getElementsByType() to query for triangles only?
-  gmsh::model::mesh::getElements(eetypes, eetags, nntags);
-  int selecttriangles = 2;
+  // element types are selected by the number of dimensions of that elements. E.g., integer 2 for
+  // triangles.
+  int selecttriangles = 2; // dimensions
+  gmsh::model::mesh::getElements(
+      eetypes,
+      eetags,
+      nntags,
+      selecttriangles, // dimension
+      -1); // select all elements with respect to their tag
+  // When calling gmsh::getElements() with a dimension argument, then the vectors
+  // eetypes, eetags and nntags are of size 1.
+  assert(eetypes.size() == 1 && eetags.size() == 1 && nntags.size() == 1 && "Assumptions not met");
+  int selectresult = 0;
+  // Sanity check
+  assert(eetags[selectresult].size() * 3 == nntags[selectresult].size() && "Size missmatch in triangle data");
 
-  size_t numtriangles = eetags[selecttriangles].size();
+  BOOST_LOG_SEV(man.lg, blt::debug) << "Number of triangles: " << eetags[selectresult].size();
+
+  size_t numtriangles = eetags[selectresult].size();
   rti::Utils::Triangle* ttbuffer = (rti::Utils::Triangle*)rtcSetNewGeometryBuffer(
       geometry,
       RTC_BUFFER_TYPE_INDEX,
@@ -187,13 +275,29 @@ int main(int argc, char* argv[]) {
 
   // Write triangle from Gmsh to Embree
   for (size_t idx = 0; idx < numtriangles; ++idx) {
-    ttbuffer[idx].v0 = nntags[selecttriangles][3*idx];
-    ttbuffer[idx].v1 = nntags[selecttriangles][3*idx+1];
-    ttbuffer[idx].v2 = nntags[selecttriangles][3*idx+2];
+    size_t ntidx = 3 * idx;
+    ttbuffer[idx].v0 = nntags[selectresult][ntidx];
+    ttbuffer[idx].v1 = nntags[selectresult][ntidx+1];
+    ttbuffer[idx].v2 = nntags[selectresult][ntidx+2];
+    assert(vvtagsminval <= ttbuffer[idx].v0 && ttbuffer[idx].v0 <= vvtagsmaxval && "Invalid Vertex");
+    assert(vvtagsminval <= ttbuffer[idx].v1 && ttbuffer[idx].v1 <= vvtagsmaxval && "Invalid Vertex");
+    assert(vvtagsminval <= ttbuffer[idx].v2 && ttbuffer[idx].v2 <= vvtagsmaxval && "Invalid Vertex");
+    ttbuffer[idx].v0 -= 1; // FIXME
+    ttbuffer[idx].v1 -= 1; // FIXME
+    ttbuffer[idx].v2 -= 1; // FIXME
+    BOOST_LOG_SEV(man.lg, blt::debug) << "(" << vvbuffer[ttbuffer[idx].v0].xx
+                                      << "," << vvbuffer[ttbuffer[idx].v0].yy
+                                      << "," << vvbuffer[ttbuffer[idx].v0].zz << ")"
+                                      << "(" << vvbuffer[ttbuffer[idx].v1].xx
+                                      << "," << vvbuffer[ttbuffer[idx].v1].yy
+                                      << "," << vvbuffer[ttbuffer[idx].v1].zz << ")"
+                                      << "(" << vvbuffer[ttbuffer[idx].v2].xx
+                                      << "," << vvbuffer[ttbuffer[idx].v2].yy
+                                      << "," << vvbuffer[ttbuffer[idx].v2].zz << ")";
   }
     
   // Invert surface normals.
-  rti::Utils::invertnormals(ttbuffer, numtriangles);
+  rti::Utils::invertnormals(ttbuffer, numtriangles); //TODO
 
   rtcCommitGeometry(geometry);
   auto geomID = rtcAttachGeometry(scene, geometry);
@@ -209,10 +313,33 @@ int main(int argc, char* argv[]) {
   RTCIntersectContext context;
   rtcInitIntersectContext(&context);
 
-  RTCRay ray = rti::Utils::getRay(0,0,0,1,1,1); //TODO
-  RTCRayHit rayhit = rti::Utils::getRayHit(ray, RTCHit{}); // TODO
+  //RTCRay ray = rti::Utils::constructRay(0,0,1, 1,1,-1); // origin (0,0,1) direction (1,1,-1)
+  //RTCRay ray = rti::Utils::constructRay(0,0,1, 1,0,-1); // origin (0,0,1) direction (1,0,-1)
+  //RTCRay ray = rti::Utils::constructRay(0,0,0.5, 1,0,0);
+  RTCRay ray = rti::Utils::constructRay(0,0,0.5, 1,0.1,0); // origin (0,0,1) direction (1,1,-1)
+  RTCRayHit rayhit = rti::Utils::constructRayHit(ray, rti::Utils::constructHit()); // TODO
+  BOOST_LOG_SEV(man.lg, blt::trace) << "Before rtcIntersect1()";
+  BOOST_LOG_SEV(man.lg, blt::trace) <<  "rayhit.ray.tfar=" << rayhit.ray.tfar;
   // TODO: According to gdb there is an error in the following call.
   rtcIntersect1(scene, &context, &rayhit);
+  BOOST_LOG_SEV(man.lg, blt::trace) << "After rtcIntersect1()";
+  BOOST_LOG_SEV(man.lg, blt::trace) <<  "rayhit.ray.tnear=" << rayhit.ray.tnear
+                                    << " rayhit.ray.tfar=" << rayhit.ray.tfar
+                                    << " rayhit.hit.Ng_x=" << rayhit.hit.Ng_x
+                                    << " rayhit.hit.Ng_y=" << rayhit.hit.Ng_y
+                                    << " rayhit.hit.Ng_z=" << rayhit.hit.Ng_z
+                                    << " rayhit.hit.primID=" << rayhit.hit.primID;
+  unsigned int primID = rayhit.hit.primID;
+  BOOST_LOG_SEV(man.lg, blt::trace) << "hit-primID=" << primID
+                                    << " (" << vvbuffer[ttbuffer[primID].v0].xx
+                                    << " ," << vvbuffer[ttbuffer[primID].v0].yy
+                                    << " ," << vvbuffer[ttbuffer[primID].v0].zz << ")"
+                                    <<  "(" << vvbuffer[ttbuffer[primID].v1].xx
+                                    << " ," << vvbuffer[ttbuffer[primID].v1].yy
+                                    << " ," << vvbuffer[ttbuffer[primID].v1].zz << ")"
+                                    <<  "(" << vvbuffer[ttbuffer[primID].v2].xx
+                                    << " ," << vvbuffer[ttbuffer[primID].v2].yy
+                                    << " ," << vvbuffer[ttbuffer[primID].v2].zz << ")";
 
   gmsh::fltk::run();
   gmsh::finalize();
--- src/rti/Utils.h
+++ src/rti/Utils.h
@@ -2,6 +2,7 @@
 #include <embree3/rtcore.h>
 
 #include <iostream>
+#include <limits>
 
 #include <assert.h>
 
@@ -20,6 +21,7 @@ class Utils {
   //struct alignas(4) Triangle {
   struct Triangle {
     uint32_t v0, v1, v2;
+    //int v0, v1, v2;
     // "RTC_GEOMETRY_TYPE_TRIANGLE: The index buffer contains an array of three
     // 32-bit indices per triangle (RTC_FORMAT_UINT format)"
     // Source: https://embree.github.io/api.html#rtc_geometry_type_triangle
@@ -29,7 +31,7 @@ class Utils {
       std::swap<uint32_t>(ptriangles[idx].v1, ptriangles[idx].v2);
     }
   }
-  static RTCRay getRay(
+  static RTCRay constructRay(
       float org_x, float org_y, float org_z,
       float dir_x, float dir_y, float dir_z) {
     RTCRay ray;
@@ -39,10 +41,23 @@ class Utils {
     ray.dir_x = dir_x;
     ray.dir_y = dir_y;
     ray.dir_z = dir_z;
+    // start of ray
+    ray.tnear = 0;
+    // Maximum length of ray
+    ray.tfar = std::numeric_limits<float>::max();
     // TODO: set other necessary fields
     return ray;
+    // "When no intersection is found, the ray/hit data is not updated. When an intersection is
+    // found, the hit distance is written into the tfar member of the ray and all hit data is set,
+    // such as unnormalized geometry normal in object space (Ng hit member), local hit coordinates
+    // (u, v hit member), instance ID (instID hit member), geometry ID (geomID hit member), and
+    // primitive ID (primID hit member). See Section RTCHit for the hit layout description."
+    // Source: https://www.embree.org/api.html#rtcintersect1
   }
-  static RTCRayHit getRayHit(RTCRay ray, RTCHit hit) {
+  static RTCHit constructHit() {
+    return RTCHit{};
+  }
+  static RTCRayHit constructRayHit(RTCRay ray, RTCHit hit) {
     return RTCRayHit{ray, hit};
   }
 };
