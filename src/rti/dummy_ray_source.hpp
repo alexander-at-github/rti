8846762fc877fea0fa4d090953eb10b382a57f42
Alexander Scharinger <alexander.at.github@gmail.com>
2019-05-15 17:51:51 +0200

    improve ray source

--- src/rti/dummy_ray_source.hpp
+++ src/rti/dummy_ray_source.hpp
@@ -1,9 +1,28 @@
 #pragma once
 
+#include <embree3/rtcore.h>
+
 #include "rti/i_ray_source.hpp"
 
 namespace rti {
   class dummy_ray_source : public i_ray_source {
-    
+  public:
+    RTCRay get_ray() {
+      RTCRay ray;
+      // Origin:
+      ray.org_x = 0;
+      ray.org_y = 0;
+      ray.org_z = 0;
+      // Direction (may not be 0 0 0)
+      ray.dir_x = 1;
+      ray.dir_y = 1;
+      ray.dir_z = 1;
+      // start of ray
+      ray.tnear = 0;
+      // Maximum length of ray
+      ray.tfar = std::numeric_limits<float>::max();
+      BOOST_LOG_SEV(rti::mRLogger, blt::error) << "ERROR: This is a dummy ray source.";
+      return ray;
+    }
   };
 }
--- src/rti/i_ray_source.hpp
+++ src/rti/i_ray_source.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
+//#include<embree3/rtcore.h>
+
 namespace rti {
   class i_ray_source {
   public:
     // Interface
     virtual~i_ray_source() {}
     // TODO: all functions added must be pure virtual
+    virtual RTCRay get_ray() = 0;
   };
 } // namespace rti
