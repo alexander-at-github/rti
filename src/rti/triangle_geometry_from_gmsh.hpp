b66435a4c126461b35fc147683364334d358d1f8
Alexander Scharinger <alexander.at.github@gmail.com>
2019-05-15 12:44:05 +0200

    Improve triangle_geometry_from_gmsh human readable messages

--- src/rti/triangle_geometry_from_gmsh.hpp
+++ src/rti/triangle_geometry_from_gmsh.hpp
@@ -23,15 +23,12 @@ namespace rti {
       }
     }
     std::string to_string() override {
-      BOOST_LOG_SEV(rti::mRLogger, blt::error)
-        << "ERROR: rti::triangle_geometry_from_gmsh::to_string() not implemented";
-      //assert(false && "not implemented")
       std::stringstream strstream;
       strstream << "(:class triangle_geometry_from_gmsh ";
       for (size_t idxA = 0; idxA < mNumTriangles; ++idxA) {
         strstream << this->prim_to_string(idxA);
         if (idxA < mNumTriangles-1) {
-          strstream << ",";
+          strstream << " ";
         }
       }
       strstream << ")";
@@ -40,14 +37,14 @@ namespace rti {
     std::string prim_to_string(unsigned int pPrimID) override {
       std::stringstream strstream;
       strstream << "(" << mVVBuffer[mTTBuffer[pPrimID].v0].xx
-                << "," << mVVBuffer[mTTBuffer[pPrimID].v0].yy
-                << "," << mVVBuffer[mTTBuffer[pPrimID].v0].zz << ")"
+                << " " << mVVBuffer[mTTBuffer[pPrimID].v0].yy
+                << " " << mVVBuffer[mTTBuffer[pPrimID].v0].zz << ")"
                 << "(" << mVVBuffer[mTTBuffer[pPrimID].v1].xx
-                << "," << mVVBuffer[mTTBuffer[pPrimID].v1].yy
-                << "," << mVVBuffer[mTTBuffer[pPrimID].v1].zz << ")"
+                << " " << mVVBuffer[mTTBuffer[pPrimID].v1].yy
+                << " " << mVVBuffer[mTTBuffer[pPrimID].v1].zz << ")"
                 << "(" << mVVBuffer[mTTBuffer[pPrimID].v2].xx
-                << "," << mVVBuffer[mTTBuffer[pPrimID].v2].yy
-                << "," << mVVBuffer[mTTBuffer[pPrimID].v2].zz << ")";
+                << " " << mVVBuffer[mTTBuffer[pPrimID].v2].yy
+                << " " << mVVBuffer[mTTBuffer[pPrimID].v2].zz << ")";
       return strstream.str();
     }
   private:
