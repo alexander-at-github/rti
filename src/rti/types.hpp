ba6700b22ba0490cb6aaa97065b94bd02c2bcf68
Alexander Scharinger <alexander.at.github@gmail.com>
2019-05-27 11:07:37 +0200

    Remove dead code

--- src/rti/types.hpp
+++ src/rti/types.hpp
@@ -13,34 +13,9 @@ namespace rti {
     T frst, scnd, thrd;
     // A hack to not need to implement the iterator, which is really
     // complicated in C++.
+    // This iterator cannot modify the content.
     std::vector<T> getIterable() {
-      // TODO: test whether you can modify the content with that.
       return {frst, scnd, thrd};
     }
   };
-
-  // template<typename T>
-  // class triple_perfect {
-  // public:
-  //   T frst, scnd, thrd;
-  //   Iterator iter() {
-  //     return {frst, scnd, thrd}.iter();
-  //   }
-  // }
-
-  // template<typename T>
-  // class triple_e : public std::tuple<T, T, T> {
-  // public:
-  //   T gFrst() {
-  //     std::get<0>(*this);
-  //   }
-  //   void sFrst(T v) {
-      
-  //   }
-  // };
-
-  // template<typename T>
-  // class triple_ee : public std::array<T, 3> {
-  //   z
-  // }
 } // namespace rti
