8ac437febfb7eb68adf4a773a6035e40489a08ed
Alexander Scharinger <alexander.at.github@gmail.com>
2019-06-05 11:33:54 +0200

    New timer based on std::chrono, may replace boost::timer

---
+++ src/rti/timer.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <chrono>
+
+namespace rti {
+  class timer {
+  public:
+    timer() :
+      mStartTime(time_stamp_now())
+    {}
+
+    void restart() {
+      mStartTime = time_stamp_now();
+    }
+
+    double elapsed_secounds() const {
+      return double(time_stamp_now() - mStartTime) * 1e-9;
+    }
+
+    std::uint64_t elapsed_nanoseconds() const {
+      return time_stamp_now() - mStartTime;
+    }
+
+  protected:
+    static std::uint64_t time_stamp_now() {
+      return std::chrono::duration_cast<std::chrono::nanoseconds>
+        (std::chrono::steady_clock::now().time_since_epoch()).count();
+    }
+  private:
+    std::uint64_t mStartTime;
+  };
+} // namespace rti
