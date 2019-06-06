9953100608f0c5a831a91dc861a58101b692de07
Alexander Scharinger <alexander.at.github@gmail.com>
2019-06-06 17:09:35 +0200

    Add origin and direction to compose source

---
+++ src/rti/i_direction.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace rti {
+  class i_direction {
+    // CONTINUE HERE
+    // Specifies a direction (independent of an origin)
+    // Gives a way to obtain a concret direction (e.g. in a random manner)
+  }
+} // namespace rti
---
+++ src/rti/i_origin.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace rti {
+  class i_origin {
+    // CONTINUE HERE
+    // Specifies a origin, that is, a subset of R^3
+    // Gives a way to obtain a location from the origin (e.g. in a random manner)
+  };
+} // namespace rti
