6435aea63f3859e83c62096dbafc1627fe6f48db
Alexander Scharinger <alexander.at.github@gmail.com>
2019-06-14 17:20:07 +0200

    Fix build script for benchmarks with intel compiler

--- benchmarks/build.sh
+++ benchmarks/build.sh
@@ -14,6 +14,9 @@ for dir in ./do/*/; do
   rm -r build.ninja CMakeCache.txt CMakeFiles/ cmake_install.cmake external/ rtiex rti-prefix/ rules.ninja .ninja*
   cmake -DCMAKE_BUILD_TYPE=Release -G Ninja ../../..
   #cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9 -G Ninja ../../..
+  # Note: when you use the intel compiler, then you have to make sure that
+  # you also have the necessary link libraries in your rpath.
+  #export LD_LIBRARY_PATH=/home/alexander/local/opt/intel/system_studio_2019/compilers_and_libraries_2019.4.235/linux/compiler/lib/intel64_lin:$LD_LIBRARY_PATH
   #cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/home/alexander/local/opt/intel/system_studio_2019/bin/icc -DCMAKE_CXX_COMPILER=/home/alexander/local/opt/intel/system_studio_2019/bin/icc -G Ninja ../../..
   cmake --build . --target rti
 done
