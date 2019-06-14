e80e38814895099fd1231a80611eae9ec187edd4
Alexander Scharinger <alexander.at.github@gmail.com>
2019-06-14 17:10:11 +0200

    Add options for compiler to benchmark script

--- benchmarks/build.sh
+++ benchmarks/build.sh
@@ -13,5 +13,7 @@ for dir in ./do/*/; do
   cd "${dir}"
   rm -r build.ninja CMakeCache.txt CMakeFiles/ cmake_install.cmake external/ rtiex rti-prefix/ rules.ninja .ninja*
   cmake -DCMAKE_BUILD_TYPE=Release -G Ninja ../../..
+  #cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9 -G Ninja ../../..
+  #cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/home/alexander/local/opt/intel/system_studio_2019/bin/icc -DCMAKE_CXX_COMPILER=/home/alexander/local/opt/intel/system_studio_2019/bin/icc -G Ninja ../../..
   cmake --build . --target rti
 done
