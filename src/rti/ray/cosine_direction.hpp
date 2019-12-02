4c0438aeb49e0f7d4a0d134dc459a326dd263c69
Alexander Scharinger <alexander.at.github@gmail.com>
2019-12-02 11:00:09 +0100

    Normalize basis on construction of cosine-direction object

--- src/rti/ray/cosine_direction.hpp
+++ src/rti/ray/cosine_direction.hpp
@@ -2,13 +2,20 @@
 
 #include "rti/ray/cos_hemi.hpp"
 #include "rti/ray/i_direction.hpp"
+#include "rti/util/utils.hpp"
 
 namespace rti { namespace ray {
   template<typename Ty>
   class cosine_direction : public rti::ray::i_direction<Ty> {
   public:
     cosine_direction(const rti::util::triple<rti::util::triple<Ty> > pBasis) :
-      mBasis(pBasis) {}
+      mBasis(pBasis) {
+      // normalize the basis (in-place)
+      rti::util::normalize(mBasis[0]);
+      rti::util::normalize(mBasis[1]);
+      rti::util::normalize(mBasis[2]);
+    }
+
 
     rti::util::triple<Ty> get(rti::rng::i_rng& pRng, rti::rng::i_rng::i_state& pRngState) const override final {
       return rti::ray::cos_hemi::get(mBasis, pRng, pRngState);
