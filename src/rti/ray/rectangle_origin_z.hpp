b3541bba056e8e7400274adf448d0ee1a03994ec
Alexander Scharinger <alexander.at.github@gmail.com>
2021-01-08 16:34:43 +0100

    Exclude one end of domain when sampling particle locations at the source

--- src/rti/ray/rectangle_origin_z.hpp
+++ src/rti/ray/rectangle_origin_z.hpp
@@ -29,15 +29,10 @@ namespace rti { namespace ray {
       auto r2 = (Ty) pRng.get(pRngState2);
       auto rngmin = (Ty) pRng.min();
       auto rngmax = (Ty) pRng.max();
-      // std::cerr << "r1: " << rngmin << " " << r1 << " " << rngmax << std::endl;
-      // std::cerr << "r2: " << rngmin << " " << r2 << " " << rngmax << std::endl;
-      auto r1prct = (r1 - rngmin) / (rngmax - rngmin);
-      auto r2prct = (r2 - rngmin) / (rngmax - rngmin);
-      // std::cerr << r1prct << " " << r2prct << std::endl;
+      auto r1prct = (r1 - rngmin) / (rngmax - rngmin + 1); // +1 in order to exclude upper limit !
+      auto r2prct = (r2 - rngmin) / (rngmax - rngmin + 1); // +1 in order to exclude upper limit !
       Ty xx = mC1[0] + (mC2[0] - mC1[0]) * r1prct;
       Ty yy = mC1[1] + (mC2[1] - mC1[1]) * r2prct;
-      // std::cerr << mC1[0] << " " << mC2[0] << " " << xx << std::endl;
-      // std::cerr << mC1[1] << " " << mC2[1] << " " << yy << std::endl;
       return {xx, yy, mZval};
     }
 
